2d146d3addd6d5e1ac6a9d4c2831778abb2ae9d7
jhamPac <cloud@kevia.me>
2019-01-03 19:06:39 -0700

    added Array.prototype.some example

--- everyNsome.js
+++ everyNsome.js
@@ -1,5 +1,10 @@
 const numbers = [12, 5, 8, 130, 44]
 
+const melissa = { first: 'Melissa', last: 'Gallo',   isNice: true }
+const john    = { first: 'John',    last: 'Mclane',  isNice: true }
+const tabby   = { first: 'Tabitha', last: 'Hillard', isNice: false }
+const jessica = { first: 'Jessica', last: 'Parker',  isNice: false }
+
 function isBigEnough(number, index) {
   return number >= 5;
 }
@@ -19,3 +24,19 @@ if (result) {
 } else {
   console.log('no it did not pass')
 }
+
+/*
+  some example
+*/
+
+const numbers = [11, 5, 9, 131, 45]
+
+function isOdd(number, index, array) {
+  return number % 2 === 1
+}
+
+function isEven(number, index, array) {
+  return number % 2 === 0
+}
+
+numbers.some(isOdd)
