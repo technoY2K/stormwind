e6f1867f73b76b9348c1ce306f1b23e9df888cfa
jhamPac <cloud@kevia.me>
2019-01-12 18:59:15 -0700

    adding constructor function solution

--- proto.js
+++ proto.js
@@ -1,27 +1,3 @@
-const animal = {
-   eat() {
-    console.log('nom nom nom')
-  },
-
-  sleep() {
-    console.log('zzzzzz')
-  }
-}
-
-const dog = Object.create(animal);
-
-dog.breed = 'mini'
-dog.name  = 'sparky'
-
-dog.eat()
-
-const cat = Object.create(animal);
-
-cat.breed = 'tabby'
-cat.name  = 'spinx'
-
-cat.sleep()
-
 // const dog = {
 //   breed: 'mini',
 //   name: 'sparky',
@@ -73,3 +49,61 @@ cat.sleep()
 //     console.log('zzzzzz')
 //   }
 // }
+
+
+// factory function solution
+// step 1
+const animal = {
+   eat() {
+    console.log('nom nom nom')
+  },
+
+  sleep() {
+    console.log('zzzzzz')
+  }
+}
+
+const dog = Object.create(animal);
+
+dog.breed = 'mini'
+dog.name  = 'sparky'
+
+dog.eat()
+
+const cat = Object.create(animal);
+
+cat.breed = 'tabby'
+cat.name  = 'spinx'
+
+cat.sleep()
+
+// step 2
+function animalCreator(proto, attributes) {
+  return Object.assign(Object.create(proto), attributes)
+}
+
+// function constructor solution
+function Animal(name) {
+  this.name  = name
+}
+
+Animal.prototype.eat = function() {
+   console.log('nom nom nom')
+}
+
+Animal.prototype.sleep = function() {
+  console.log('zzzZZZzzz')
+}
+
+function Dog(name, breed) {
+  Animal.call(this, name)
+  this.breed = breed
+}
+
+Dog.prototype = Object.create(Animal.prototype)
+
+Dog.prototype.bark = function() {
+  console.log('woof woof')
+}
+
+Dog.prototype.constructor = Dog
