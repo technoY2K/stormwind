5a56775b66a2ea2a218cde802eec7a8d388a6b0a
jhamPac <cloud@kevia.me>
2019-02-16 20:14:11 -0700

    object example

--- es6-features/restAndSpread.js
+++ es6-features/restAndSpread.js
@@ -14,3 +14,27 @@ const oddNumbers  = [1, 3, 5]
 const evenNumbers = [2, 4, 6]
 
 const combined = [ ...evenNumbers, ...oddNumbers, 6, 7, ]
+
+// Objects
+//////////////////
+const profile = {
+  first: 'Alex',
+  last: 'Casio',
+  occupation: 'secret agent',
+  hrInfo: {
+    employeeID: 17,
+    departmentID: 34,
+    payGrade: 'G:55',
+    yearsOfService: 17
+  },
+  hobbies: [
+    'drawing',
+    'fortnite',
+    'target practice',
+    'fencing',
+    'martini'
+  ],
+  address: 'Its a secret'
+}
+
+const { first, last } = profile
