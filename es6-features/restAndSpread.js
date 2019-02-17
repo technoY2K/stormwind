0f7b397554c07d95e2e7de0dd657b9563705f373
jhamPac <cloud@kevia.me>
2019-02-16 20:00:08 -0700

    examples of drs

--- es6-features/restAndSpread.js
+++ es6-features/restAndSpread.js
@@ -1,1 +1,9 @@
-// silence is golden
+// Arrays
+const numbers = [1, 2]
+
+const [one, two] = numbers
+
+// better default example
+const domain = 'example.org'
+
+const [host, tld = 'net'] = domain.split('.')
