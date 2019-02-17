cae4e6f2166a2522c4e8e1c4982ecf21e99dac8f
jhamPac <cloud@kevia.me>
2019-02-16 20:04:30 -0700

    rest

--- es6-features/restAndSpread.js
+++ es6-features/restAndSpread.js
@@ -1,9 +1,16 @@
 // Arrays
-const numbers = [1, 2]
+const numbers = [1, 2, 3, 4, 5, 6, 7]
 
-const [one, two] = numbers
+// rest example
+const [one, two, ...rest] = numbers
 
 // better default example
 const domain = 'example.org'
 
 const [host, tld = 'net'] = domain.split('.')
+
+// spread
+const oddNumbers  = [1, 3, 5]
+const evenNumbers = [2, 4, 6]
+
+const combined = [ ...evenNumbers, ...oddNumbers, 6, 7, ]
